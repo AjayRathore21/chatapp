7196db1c88a2876a2e4fd252fa81164760ea577a
Ajay Rathore <ajaykumar420.ak79@gmail.com>
2023-06-09 17:05:20 +0530

    app design completed!!

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Navbar = () => {
+  
+
+  return (
+    <div className="navbar">
+      <span className="logo">ChatApp</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>hllo</span>
+        <button >logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="container">
-       <h1>Home</h1>
+        <Sidebar />
+        <Chat />
       </div>
     </div>
   );
