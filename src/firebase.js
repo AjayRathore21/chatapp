4f8e77665dc9e6b5f4f7f8a97f7fdd68b0e44a4a
Ajay Rathore <ajaykumar420.ak79@gmail.com>
2023-06-09 13:49:49 +0530

    firebase practice and project setted up for development!

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import {getAuth} from 'firebase/Auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCduroyCMltI_l-ehArNXWmWugMDLJ7zvk",
+  authDomain: "react-chatapp-5c9c8.firebaseapp.com",
+  projectId: "react-chatapp-5c9c8",
+  storageBucket: "react-chatapp-5c9c8.appspot.com",
+  messagingSenderId: "598671020925",
+  appId: "1:598671020925:web:d9ebf278b492d5e1823214",
+  measurementId: "G-FHBL70T5RE",
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
+
+
